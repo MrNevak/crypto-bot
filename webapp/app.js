30632aad57ba2cf2f7a7b743e7eda14520c29018
MrNevak <davidfarmll628403@gmail.com>
2026-04-04 01:08:44 +0300

    Update app.js

--- webapp/app.js
+++ webapp/app.js
@@ -1,14 +1,11 @@
 let tg = window.Telegram.WebApp;
 tg.expand();
 
-console.log('App started');
-
 function showOptions() {
     document.getElementById('welcomeScreen').style.display = 'none';
     document.getElementById('optionsScreen').style.display = 'block';
 }
 
 document.getElementById('startWorkBtn').onclick = function() {
-    alert('Button clicked');
     showOptions();
 };
